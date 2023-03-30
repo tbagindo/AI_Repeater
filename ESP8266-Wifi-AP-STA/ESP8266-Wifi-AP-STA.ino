dcbe8853a5ac073667cc5ba99b42099410f685f6
ApaITU channel <77989568+tbagindo@users.noreply.github.com>
2023-03-31 05:50:44 +0700

    Update ESP8266-Wifi-AP-STA.ino

--- ESP8266-Wifi-AP-STA/ESP8266-Wifi-AP-STA.ino
+++ ESP8266-Wifi-AP-STA/ESP8266-Wifi-AP-STA.ino
@@ -6,6 +6,27 @@
  * 
  * otherwise the traffic from main router side 
  * will not route to 192.168.4.0  network
+ * 
+ * there is a flaw in library, by default dhcp server will set the softAPIP 
+ * (interface ip) as the dns server. Where there is no dns service running on it host.
+ * as workaround  we have two choices :
+ * a) run dns server on esp8266
+ * b) set the ip correct ip of dns server
+ * we prefer the second option
+ *
+ * for esp8266 core 3.0.x :
+ * use LwipDhcpServer.h library
+ * #include <LwipDhcpServer.h>
+ * in void setup() 
+ * set the dns ip address of station side dns as the  entry
+ * dhcpSoftAP.dhcps_set_dns(0, WiFi.dnsIP(0));
+ * dhcpSoftAP.dhcps_set_dns(1, WiFi.dnsIP(1));
+ *
+ * for esp8266 core 3.1.x
+ * add following line in void setup() 
+ * auto& dserver = WiFi.softAPDhcpServer();
+ * dserver.setDns(WiFi.dnsIP(0));
+ * we could only set single dns entry in this version
  */
 
 #include <ESP8266WiFi.h>
@@ -44,6 +65,10 @@ void setup(){
   Serial.println(WiFi.localIP());
 
   //Access Point
+  // esp8266 core 3.1.x dns entry
+  auto& dserver = WiFi.softAPDhcpServer();
+  dserver.setDns(WiFi.dnsIP(0));
+
   WiFi.softAP(SSID,PASS);
   Serial.println(WiFi.softAPIP());
   
