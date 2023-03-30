71fd70e01c7ac36fb18eaa885fa15e4cd6962be7
ApaITU channel <77989568+tbagindo@users.noreply.github.com>
2023-03-31 05:47:35 +0700

    Update ESP8266-Wifi-AP-STA-IPaddress.ino

--- ESP8266-Wifi-AP-STA-IPaddress/ESP8266-Wifi-AP-STA-IPaddress.ino
+++ ESP8266-Wifi-AP-STA-IPaddress/ESP8266-Wifi-AP-STA-IPaddress.ino
@@ -7,18 +7,32 @@
  * otherwise the traffic from main router side 
  * will not route to 192.168.4.0  network
  * 
- * there is a bug in library, if we config custom network other  than
- * 192.168.4.1, the dhcp server will not share ip adress of dns server
- * as workaround  we use LwipDhcpServer.h library
- * and set the dns ip address of station side dns as the  entry
+ * there is a flaw in library, by default dhcp server will set the softAPIP 
+ * (interface ip) as the dns server. Where there is no dns service running on it host.
+ * as workaround  we have two choices :
+ * a) run dns server on esp8266
+ * b) set the ip correct ip of dns server
+ * we prefer the second option
+ *
+ * for esp8266 core 3.0.x :
+ * use LwipDhcpServer.h library
+ * #include <LwipDhcpServer.h>
+ * in void setup() 
+ * set the dns ip address of station side dns as the  entry
  * dhcpSoftAP.dhcps_set_dns(0, WiFi.dnsIP(0));
  * dhcpSoftAP.dhcps_set_dns(1, WiFi.dnsIP(1));
+ *
+ * for esp8266 core 3.1.x
+ * add following line in void setup() 
+ * auto& dserver = WiFi.softAPDhcpServer();
+ * dserver.setDns(WiFi.dnsIP(0));
+ * we could only set single dns entry in this version
  */
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-//#include <LwipDhcpServer.h>
+#include <LwipDhcpServer.h> 
 
 //Station SSID
 const char *stSSID = "mktApaITU";
@@ -56,10 +70,14 @@ void setup(){
   Serial.println(WiFi.localIP());
 
   //Access Point
-  /*
+  // esp8266 core version 3.0.x
   dhcpSoftAP.dhcps_set_dns(0, WiFi.dnsIP(0));
   dhcpSoftAP.dhcps_set_dns(1, WiFi.dnsIP(1));
-  */  
+  
+  // esp8266 core versio. 3.1.x
+  // auto& dserver = WiFi.softAPDhcpServer();
+  // dserver.setDns(WiFi.dnsIP(0));
+
   WiFi.softAPConfig(local_IP,gateway,subnet);
   WiFi.softAP(SSID,PASS);
   Serial.println(WiFi.softAPIP());
